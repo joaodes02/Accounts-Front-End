9f3d3194a13e6a2d4e6e5c5fc4ec2225f91da78b
João VIctor Silva <joaovictordes02@gmail.com>
2024-07-29 20:30:27 -0300

    Adicionando Usuarios manualmente

--- front-end-API/src/pages/home/index.jsx
+++ front-end-API/src/pages/home/index.jsx
@@ -1,14 +1,49 @@
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
 
+  const users = [{
+    id: '1231231sdasd',
+    name: 'Joao',
+    age: "23",
+    email: 'joao@gmail.com'
+  },
+  {
+    id:'123123134ssssss',
+    name:'Aleixo',
+    age: '24',
+    email: 'aleixo@gmail.com'
+
+  }]
+
   return (
     
-      <div>
-        <h1>Olá React</h1>
-      </div>
-       
-    
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuarios</h1>
+        <input name='nome' type='text'/>
+        <input name='idade' type='number'/>
+        <input name='email'type='email'/>
+        <button type='button'>Cadastrar</button>
+      </form>
+
+
+    {users.map(user => (
+      <div key={user.id}>
+        <div>
+            <p>Nome: {user.name}</p>
+            <p>Idade: {user.age} </p>
+            <p>E-mail: {user.email}</p>
+        </div>
+            <button>
+              <img src={Trash}/>
+            </button>
+        </div>
+   ))}
+  </div>
+      
+         
   )
 }
 
