61ecec3beb3894eb5c9e5b572865877af52d2846
João VIctor Silva <joaovictordes02@gmail.com>
2024-07-29 22:08:29 -0300

    Conexão com o DB + Funções

--- front-end-API/src/pages/home/index.jsx
+++ front-end-API/src/pages/home/index.jsx
@@ -1,31 +1,49 @@
+import react from 'react'
 import './style.css'
 import Trash from '../../assets/trash.svg'
+import api from '../../services/api'
+import { useEffect, useState, useRef } from 'react'
 
 function Home() {
 
-  const users = [{
-    id: '1231231sdasd',
-    name: 'Joao',
-    age: "23",
-    email: 'joao@gmail.com'
-  },
-  {
-    id:'123123134ssssss',
-    name:'Aleixo',
-    age: '24',
-    email: 'aleixo@gmail.com'
+  const [users, setUsers] = useState([])
 
-  }]
+  const inputName = useRef()
+  const inputIdade = useRef()
+  const inputEmail = useRef()
+
+  async function getUsers(){
+    const usersFromApi = await api.get('/usuarios') 
+    setUsers(usersFromApi.data)
+   
+  }
+
+  async function createUsers(){
+    await api.post('/usuarios',{
+      name: inputName.current.value,
+      age: inputIdade.current.value,
+      email: inputEmail.current.value
+    })
+    getUsers();
+  }
+
+
+  useEffect(()=>{
+    getUsers();
+
+  }, [])
+  
+
 
   return (
     
     <div className='container'>
       <form>
         <h1>Cadastro de Usuarios</h1>
-        <input placeholder="Nome" name='nome' type='text'/>
-        <input  placeholder= "Idade" name='idade' type='number'/>
-        <input placeholder="E-mail" name='email'type='email'/>
-        <button type='button'>Cadastrar</button>
+        <input placeholder="Nome" name='nome' type='text' ref={inputName}/>
+        <input  placeholder= "Idade" name='idade' type='number' ref={inputIdade}/>
+        <input placeholder="E-mail" name='email'type='email' ref={inputEmail}/>
+        <button type='button' onClick={createUsers}>Cadastrar</button>
       </form>
 
 
